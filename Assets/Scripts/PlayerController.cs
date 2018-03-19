2bea65418e955889980be2b762b63a112c66d308
Taaro Bravo <lautabdls@gmail.com>
2018-03-19 19:18:43 -0300

    Movimientos Básicos
    -Movimientos para desplazarse
    -Salto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveVector;
+    public float gravity;
+    public float verticalVelocity;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    { 
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = 0f;
+        
+        controller.Move(moveVector * Time.deltaTime);
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (!controller.isGrounded && Vector3.Angle(transform.up, hit.normal) == 180)
+        {
+            if (verticalVelocity != 0)
+                verticalVelocity = 0;
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+    }
+}
