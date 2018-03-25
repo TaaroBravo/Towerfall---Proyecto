af9cdde056038029c88347beb33ce826ce4a4762
Taaro Bravo <lautabdls@gmail.com>
2018-03-25 01:29:11 -0300

    Mecanica Dash V1.0
    Dash funcionando.
    -Ver si hay errores o problematicas.
    -Falta implementar SOLID. Cuando haga un ataque al menos, lo implemento con todo.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,14 +13,32 @@ public class PlayerController : MonoBehaviour
     public float gravity;
     public float verticalVelocity;
 
+    public float dashDistance;
+    public float dashSpeed;
+    public float dashTimer;
+    public float dashingTime;
+    public bool isDashing;
+
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        dashSpeed = 50;
+        dashDistance = 7;
+        dashTimer = dashDistance / dashSpeed;
     }
 
     void Update()
     { 
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            isDashing = true;
+            moveVector.x = Mathf.Sign(moveVector.x) * dashSpeed;
+            verticalVelocity = 0;
+            moveVector.y = verticalVelocity;
+            moveVector.z = 0f;
+        }
 
         if (controller.isGrounded)
         {
@@ -35,10 +53,21 @@ public class PlayerController : MonoBehaviour
             verticalVelocity -= gravity * Time.deltaTime;
         }
 
-        moveVector = Vector3.zero;
-        moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
-        moveVector.y = verticalVelocity;
-        moveVector.z = 0f;
+        if(isDashing && dashingTime <= dashTimer)
+        {
+            dashingTime += Time.deltaTime;
+        }
+
+        else
+        {
+            if (controller.isGrounded)
+                isDashing = false;
+            dashingTime = 0;
+            moveVector = Vector3.zero;
+            moveVector.x = Input.GetAxis("Horizontal") * moveSpeed;
+            moveVector.y = verticalVelocity;
+            moveVector.z = 0f;
+        }
         
         controller.Move(moveVector * Time.deltaTime);
 
@@ -48,6 +77,10 @@ public class PlayerController : MonoBehaviour
     {
         if (!controller.isGrounded && Vector3.Angle(transform.up, hit.normal) == 180)
         {
+            //Opcion I
+            //verticalVelocity = -Mathf.Abs(verticalVelocity / 2);
+
+            //Opcion II
             if (verticalVelocity != 0)
                 verticalVelocity = 0;
             verticalVelocity -= gravity * Time.deltaTime;
